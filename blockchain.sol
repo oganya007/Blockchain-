dc38df833f3db7f755c12d43635e98bbf663e013
oganya007 <oganyafelicia@gmail.com>
2022-09-23 15:27:12 +0100

    first commit

---
+++ blockchain.sol
@@ -0,0 +1,29 @@
+blockchain.sol
+grace.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract fay{
+
+     struct school{
+         string name
+         uint age
+         string height
+     }
+
+     school education = school("Felicia", 12, "6foot");
+
+//dynamic array
+    school[] marianhill 
+
+    //push into array
+    marianhill.push[1]
+    marianhill.push[2]
+    marianhill.push[3]
+    marianhill.push[4]
+    marianhill.push[5]
+
+    struct Winner
+    
+    
+}
